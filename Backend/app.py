0f8d517e3ac7100bd2cdae313fc542d4e054f9d7
busi-reddy-karnati <bk2444@nyu.edu>
2023-10-31 19:52:18 -0400

    UPD: Authentication a

--- Backend/app.py
+++ Backend/app.py
@@ -1,13 +1,19 @@
-from flask import Flask
+from flask import Flask, json
+from handlers.authentication import LoginHandler
+from flask import request
 
 app = Flask(__name__)
 
-
+login_handler = LoginHandler()
+assert login_handler.validate_user("test","test")
+@app.route('/', methods=['POST'])
 def home():
     user_name = request.form['user_name']
     password = request.form['password']
     if login_handler.validate_user(user_name, password):
-
+        print("OK")
+        return json.dumps({'success':True}), 200, {'ContentType':'application/json'}
+    return "Failed Login"
 
 if __name__ == '__main__':
     app.run()
--- Backend/handlers/authentication.py
+++ Backend/handlers/authentication.py
@@ -5,12 +5,17 @@ class LoginHandler:
         pass
 
     def validate_user(self, user_name, password):
+        # todo: Resolve problem with connection. Update.
         con = sql.connect("../database/database.db")
         cur = con.cursor()
         assert isinstance(user_name, str)
         assert isinstance(password, str)
-        cur.execute(f"select * from login_details where user_id={user_name} and password={password}")
+        cur.execute("select * from login_details where user_id=? and password=?",(user_name, password))
         user = cur.fetchone()
         if not user:
             return False
         return True
+
+# todo: Change to central test location
+test_login_handler = LoginHandler()
+assert test_login_handler.validate_user("test", "test")
